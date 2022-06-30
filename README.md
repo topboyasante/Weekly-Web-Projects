69b644ed8838733b012794a74aa79acbd21ab2b6
Nana K <topboyasante@gmail.com>
2022-06-30 16:08:19 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weekly-Web-Projects
+ I try to build pages every week.
---
+++ Week 01/script.js
@@ -0,0 +1,9 @@
+let navbarLinks = document.getElementById('navbar-links');
+
+function showMenu(){
+    navbarLinks.style.right="0";
+}
+
+function hideMenu(){
+    navbarLinks.style.right="-200px";
+}
